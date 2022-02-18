de05cede3ff3a30f874eec8cd0ec7ef2931a663d
Panwar94 <99253737+Panwar94@users.noreply.github.com>
2022-02-18 15:48:12 +0530

    Add files via upload

---
+++ Python Assignment 1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. In the below elements which of them are values or an expression? eg:- values can be integer or string and expressions will be mathematical operators.
+* Expression
+'hello' Value
+-87.8 value
+- Expression
+/ Expression
++	Expression
+6 values2. What is the difference between string and variable?
+Ans= String is a collection of one or more characters put in single quote, double quote and triple quote character. Like, “sam”, “sam is a boy”.
+And variable is used to store or assign values like a= 20, b = “Pankaj”3. Describe three different data types.
+Ans= 1- Integer is a numerical data type which contains positive and negative whole number except fractions and decimal values. Like – 1,2,3,4…
+2- Float is also a numeric data type in the decimal form. Like – 34.5, 56.7
+3- String is a collection of one or more characters put in single quote, double quote and triple quote character. Like “Panwar is a sur name”, ‘12345’ 4. What is an expression made up of? What do all expressions do?
+Ans= Expressions are made up of operators and operands. For example a= (2+3*5)+24 is a statement so operators are +, *, + and 2, 3,5,24 are known as operands and the whole statement is called expression.
+
+Expressions is instruction which  evaluate the operators and operands and when print command is given, it will show some result.
+a= (1+2)
+print(a) 
+35. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+Ans= Expression always produce or return a result in the end when evaluated but statement may or may not produce a statement in the end.
+Like spam = 10 ----- Statement
+a = spam +20------ expression  also a= spam +20 is statement 
+print(a)
+30
+6. After running the following code, what does the variable bacon contain?
+bacon = 22
+bacon + 1
+22+1
+23
+Bacon variable contain 22 
+7. What should the values of the following two terms be?
+'spam' + 'spamspam' = ‘spam’ , ‘spamspam’ both are values.
+
+'spam' * 3= ‘spam’ and 3 both are values.
+8. Why is eggs a valid variable name while 100 is invalid?
+Ans= Because a variable name cannot start from any numerical value. 
+
+# In[9]:
+
+
+get_ipython().set_next_input('9. What three functions can be used to get the integer, floating-point number, or string version of a value');get_ipython().run_line_magic('pinfo', 'value')
+Ans=int (), float () and str ()
+
+10. Why does this expression cause an error? How can you fix it?
+'I have eaten ' + 99 + ' burritos.'
+Ans= This expression cannot print any result as in this statement “I have eaten ‘ and ‘burrios’ both are str type of variable and 99 is int type of variable so as per rule str and int variable cannot concatenate in python language.
+
+# In[ ]:
+
+
+
+
